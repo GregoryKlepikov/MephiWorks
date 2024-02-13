fefd4842ac3990539116478ac06e8af211a08d65
Master <gregory.klepikov@yandex.ru>
2024-02-13 15:06:53 +0300

    sad true 2

--- test1.py
+++ test1.py
@@ -1,3 +1,3 @@
 import random
 x = random.randint(15, 60)
-print("Hello! I am big boy Alexander, i make my homework without my Mummy! My iq is", x)
+print("Hello! I am big boy Alexander, i make my homework without my Mummy! My iq is", x, "I love be idiot")
